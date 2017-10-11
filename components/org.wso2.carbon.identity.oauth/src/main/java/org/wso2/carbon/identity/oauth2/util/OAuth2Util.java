9f382a0cc31c65ce6823e8e5ecdbc090864ac184
darshanasbg <darshana@wso2.com>
2017-10-12 00:28:12 +0530

    IDENTITY-6462: Adding unit tests

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/util/OAuth2Util.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/util/OAuth2Util.java
@@ -537,26 +537,18 @@ public class OAuth2Util {
         return userStoreDomainMap;
     }
 
-    public static String getMappedUserStoreDomain(String userStoreDomain) throws IdentityOAuth2Exception {
-
-        String mappedUserStoreDomain = userStoreDomain;
-
-        Map<String, String> availableDomainMappings = OAuth2Util.getAvailableUserStoreDomainMappings();
-        if (userStoreDomain != null && availableDomainMappings.containsKey(userStoreDomain)) {
-            mappedUserStoreDomain = availableDomainMappings.get(userStoreDomain);
-        }
-
-        return mappedUserStoreDomain;
-    }
-
     public static String getPartitionedTableByUserStore(String tableName, String userStoreDomain)
             throws IdentityOAuth2Exception {
 
-        if (StringUtils.isNotBlank(userStoreDomain) &&
+        if (StringUtils.isNotBlank(tableName) && StringUtils.isNotBlank(userStoreDomain) &&
                 !IdentityUtil.getPrimaryDomainName().equalsIgnoreCase(userStoreDomain)) {
 
-            String mappedUserStoreDomain = OAuth2Util.getMappedUserStoreDomain(userStoreDomain);
-            tableName = tableName + "_" + mappedUserStoreDomain;
+            Map<String, String> availableDomainMappings = OAuth2Util.getAvailableUserStoreDomainMappings();
+
+            if (userStoreDomain != null && availableDomainMappings.containsKey(userStoreDomain)) {
+                String mappedUserStoreDomain = availableDomainMappings.get(userStoreDomain);
+                tableName = tableName + "_" + mappedUserStoreDomain;
+            }
         }
 
         return tableName;
@@ -647,7 +639,12 @@ public class OAuth2Util {
         if (userId != null) {
             String[] strArr = userId.split(UserCoreConstants.DOMAIN_SEPARATOR);
             if (strArr != null && strArr.length > 1) {
-                userStoreDomain = getMappedUserStoreDomain(strArr[0]);
+                userStoreDomain = strArr[0];
+                Map<String, String> availableDomainMappings = OAuth2Util.getAvailableUserStoreDomainMappings();
+
+                if (userStoreDomain != null && availableDomainMappings.containsKey(userStoreDomain)) {
+                    userStoreDomain = availableDomainMappings.get(userStoreDomain);
+                }
             }
         }
         return userStoreDomain;
